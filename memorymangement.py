285a90693843b1216397e4802f5cf0b99e25c3ef
RacistGiraffe <znguyenmdc30@gmail.com>
2019-05-13 23:08:46 -0700

    Temporarily made blanks '0'

--- memorymangement.py
+++ memorymangement.py
@@ -1,24 +1,14 @@
 def readfile():
     memory_file = open("memory.dat", "r")
     memory = []
+    page = []
     line = memory_file.readline()
     while line:
         line = line.split()
-        memory.append(memory(int(line[0]), str(line[1]), int(line[2])))
+        memory.append(Job(int(line[0]), str(line[1]), int(line[2])))
         line = memory_file.readline()
     return memory
     #memory is an array that stores the PID, ACTION, and PAGE to take
-
-
-
-if __name__== "__main__":
-    memory = readfile()
-    pagetable = []
-    for i in range (0, len(memory)):
-        if(memory[i].action == 'A'):
-            pagetable.append(memory[i].process_id)
-    for i in range (0, len(pagetable)):
-        print(pagetable[i])
 
 class Job:
     def __init__(self, process_id, action, page):
@@ -32,8 +22,18 @@ class Job:
         self.R = None
         self.W= None
         self.F= None
-        self.pages= None
+        self.virtual = []
+        self.physical = []
 
     def __repr__(self):
         return "process_id : {}, action: {},page: {}"\
             .format(self.process_id, self.action, self.page)
+
+if __name__== "__main__":
+    memory = readfile()
+    pagetable = []
+    for i in range (0, len(memory)):
+        if(memory[i].action == 'A'):
+            pagetable.append(memory[i].process_id)
+    for i in range (0, len(pagetable)):
+        print(pagetable[i])
