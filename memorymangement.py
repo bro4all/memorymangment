b0b16bc0cce717ccc857ad49732b19ba686793aa
RacistGiraffe <znguyenmdc30@gmail.com>
2019-05-13 22:33:32 -0700

    Added some improvements to code, reading still not working

--- memorymangement.py
+++ memorymangement.py
@@ -4,10 +4,21 @@ def readfile():
     line = memory_file.readline()
     while line:
         line = line.split()
-        memory.append(memory(int(line[0]), int(line[1]), int(line[2])))
+        memory.append(memory(int(line[0]), str(line[1]), int(line[2])))
         line = memory_file.readline()
     return memory
+    #memory is an array that stores the PID, ACTION, and PAGE to take
 
+
+
+if __name__== "__main__":
+    memory = readfile()
+    pagetable = []
+    for i in range (0, len(memory)):
+        if(memory[i].action == 'A'):
+            pagetable.append(memory[i].process_id)
+    for i in range (0, len(pagetable)):
+        print(pagetable[i])
 
 class Job:
     def __init__(self, process_id, action, page):
